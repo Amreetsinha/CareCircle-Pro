4111f262e572dba2a32e278ea0e2e272c6177c14
Shivakumar yadav <shivayadavjabbu@gmail.com>
2026-01-19 16:45:21 +0530

    feat(gateway): add base security configuration for route protection
    - Enabled Spring Security for API Gateway using WebFlux
    - Allowed unauthenticated access only to /auth/** endpoints
    - Enforced authentication for all other incoming requests
    - Prepared gateway for JWT-based security enforcement

---
+++ GatewayAPI/src/main/java/com/GatewayAPI/GatewayAPI/config/Securityconfig.java
@@ -0,0 +1,39 @@
+package com.GatewayAPI.GatewayAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class Securityconfig {
+	/*
+	 * Base Security configuration for API Gateway 
+	 * 
+	 * - Disable CSRF because the gateway is stateless
+	 * - Allows public access only to authenticate endpoints 
+	 * - Secures all other routes by default
+	 * 
+	 * jwt authentication will be added in  a separate step
+	 */
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+		return http
+				// Disable CSRF because gateway is stateless and uses JWT
+				.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange(exchange -> exchange
+
+						// Public endpoints (authentication service)
+
+						.pathMatchers("/auth/**").permitAll()
+
+						/// All other routes must be authenticated
+
+						.anyExchange().authenticated())
+				.build();
+
+	}
+}
