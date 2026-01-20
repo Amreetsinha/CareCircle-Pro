d99e08621118852f78d69699ccc0a8176dbd3861
Shivakumar yadav <shivayadavjabbu@gmail.com>
2026-01-20 23:13:22 +0530

    feat(auth): add service layer for user registration and login
    - Introduced UserService interface to define auth business operations
    - Implemented UserServiceImpl with core registration and login logic
    - Centralized business rules outside controllers
    - Prepared auth service for security and database integration

---
+++ GatewayAPI/src/main/java/com/parentpulse/auth/service/UserService.java
@@ -0,0 +1,27 @@
+package com.parentpulse.auth.service;
+
+import com.parentpulse.auth.dto.request.LoginRequest;
+import com.parentpulse.auth.dto.request.RegisterRequest;
+import com.parentpulse.auth.model.User;
+
+/**
+ * Defines authentication-related business operation.
+ */
+public interface UserService {
+	
+	/*
+	 * Register a new user. 
+	 * 
+	 * @param request registration request data
+	 * @param created user
+	 */
+	
+	User register(RegisterRequest request); 
+	
+	/*
+	 * Authenticate a user
+	 * @param request login request data
+	 * @return authenticated user
+	 */
+	User login(LoginRequest request); 
+}
---
+++ GatewayAPI/src/main/java/com/parentpulse/auth/service/impl/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.parentpulse.auth.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.parentpulse.auth.dto.request.LoginRequest;
+import com.parentpulse.auth.dto.request.RegisterRequest;
+import com.parentpulse.auth.model.User;
+import com.parentpulse.auth.repository.UserRepository;
+import com.parentpulse.auth.service.UserService;
+
+
+/*
+ * Implementation of authentication business logic
+ */
+@Service
+public class UserServiceImpl implements UserService {
+	
+	private final UserRepository userRepository; 
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository; 
+	}
+
+	@Override
+	public User register(RegisterRequest request) {
+		
+		//Business rule: email must be unique
+		if (userRepository.existsByEmail(request.getEmail())) {
+			throw new IllegalStateException("User already exists with this email.");
+		}
+		
+		User user = new User(); 
+		
+		user.setEmail(request.getEmail());
+		
+		user.setPassword(request.getPassword());
+		
+        user.setRole(request.getRole());
+        
+        user.setEnabled(true);
+        
+        return userRepository.save(user);
+	}
+	
+
+	@Override
+	public User login(LoginRequest request) {
+		
+		return userRepository.findByEmail(request.getEmail())
+				.filter(User::isEnabled)
+				.orElseThrow(() ->
+				new IllegalStateException("Invalid email or password")); 
+				
+	}
+
+}
