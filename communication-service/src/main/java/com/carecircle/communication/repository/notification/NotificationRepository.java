d14a1fe696222626abd9dd6b0350ee1fc974376d
Amreetkumar sinha <amreetanshukumarsinha@gmail.com>
2026-01-26 00:03:12 +0530

    feat(notification): add Notification repository
    Introduced repository for Notification entity to
    support persistence and read/unread tracking.
    
    Delivery mechanisms will be added in later phases.

---
+++ communication-service/src/main/java/com/carecircle/communication/repository/notification/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.carecircle.communication.repository.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.carecircle.communication.model.notification.Notification;
+
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+}
