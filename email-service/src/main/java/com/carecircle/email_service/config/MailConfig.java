0bf962f7a06b105e45078193a6f5a93a42c63601
Amreet kumar sinha <amreetanshukumarsinha@gmail.com>
2026-01-30 16:55:42 +0530

    chore: remove duplicate email-service project and cleanup zip file

--- email-service/src/main/java/com/carecircle/email_service/config/MailConfig.java
+++
@@ -1,9 +0,0 @@
-package com.carecircle.email_service.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MailConfig {
-    // For now empty
-    // Spring Boot auto-configures JavaMailSender
-}
